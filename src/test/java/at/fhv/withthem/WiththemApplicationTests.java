d34001af17fda3b3e42a6e0a27087ff87256508b
neyon <neyon@gmx.at>
2024-03-27 08:54:31 +0100

    initialized project

---
+++ src/test/java/at/fhv/withthem/WiththemApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.withthem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiththemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
