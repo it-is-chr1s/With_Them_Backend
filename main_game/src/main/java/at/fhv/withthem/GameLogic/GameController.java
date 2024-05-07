c5536426d7a8992eb3b1f9b37a727f8794a14816
it-is-chr1s <Andreko@gmx.de>
2024-05-07 17:55:48 +0200

    fixed crossOrigin create game

--- main_game/src/main/java/at/fhv/withthem/GameLogic/GameController.java
+++ main_game/src/main/java/at/fhv/withthem/GameLogic/GameController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:5173")
 public class GameController {
 
     private final GameService gameService;
